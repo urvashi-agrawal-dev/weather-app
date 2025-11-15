43ff0f8788e0333a1b3e037ff6f1807cf47a23d5
Urvashi Agrawal <158180920+urvashi-agrawal-dev@users.noreply.github.com>
2025-11-15 12:41:07 +0000

    v

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const url='https://www.weatherapi.com/my/';
-const apiKey='01bae03f01254f7fbf180834251511';
+const url='https://api.openweathermap.org/data/2.5/weather';
+const apiKey='1bb92d5e18a6b6aa3fd03ea6c9e8cdae';
 $(document).ready(function(){
     weatherFn('Noida');
+    $('#city-input-btn').on('click',function(){
+        let cityName=$('#city-input').val();
+        if(cityName){
+            weatherFn(cityName);
+
+        } else {
+            alert('Please enter a city name');
+        }
+    });
 });
 async function weatherFn(cName){
     const temp=`${url}?q=${cName}&appid=${apiKey}&units=metric`;
@@ -24,13 +33,7 @@ function weatherShowFn(data){
     $('#temperature').html(`${Math.round(data.main.temp)}°C`);
     $('#description').text(data.weather[0].description);
     $('#wind-speed').html(`Wind Speed:${data.wind.speed}m/s`);
-    $('#city-input-btn').on('click',function(){
-        let cityName=$("#city-input").val();
-        if(cityName){
-            weatherShowFn(cityName);
-        } else {
-            alert("please enter a city name.");
-        }
-    });
-        $('#weather-info').fadeIn();
+    const iconCode=data.weather[0].icon;
+    $('#weather-icon').attr('src',`https://openweathermap.org/img/wn/${iconCode}@2x.png`);
+    $('#weather-info').fadeIn();
 }
